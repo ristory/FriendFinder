5dd5251764c55f646ccd81e906e0df92cb2765a8
hoang <hoangcao@mail.weber.edu>
2018-11-01 21:32:06 -0600

    finish /friendfinder

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+var friendArray = [
+    {
+        name:"Songoku",
+        photo:"https://i.pinimg.com/originals/68/39/f8/6839f871b1a83e72123eb28fca9364a5.jpg",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    },
+
+    {
+        name:"Vegeta",
+        photo:"https://vignette.wikia.nocookie.net/dragonballfanon/images/3/36/Super_Saiyan_Vegeta_%28Xz%29.jpg/revision/latest?cb=20130826092537",
+        scores:[
+            2,
+            1,
+            2,
+            4,
+            2,
+            1,
+            2,
+            2,
+            4,
+            2
+          ]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,28 @@
+var friends = require("../data/friends");
 module.exports = function(app) {
-}
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+      });
+
+      app.post('/api/friends', function(req, res) {
+        friends.push(req.body);
+        res.json(true);
+        var name;
+        var image;
+        var diff = 0;
+
+        for (i = 0; i <friends.length; i++)
+        {
+            for (j = 0; j <friends[i].length; j++)
+            {
+                diff += Math.abs(friends[i].scores[j] - friends[i + 1].scores[j]);
+                var totaldiff = [];
+                totaldiff.push(diff);
+                var a = Math.max(...totaldiff);
+                var b = totaldiff.indexOf(a);
+                name = friends[b].name
+                image = friends[b].photo
+            }
+        }
+      });
+};
